17f95d5fe6c3a07057818518786dfb1e84b041e9
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-11-06 20:39:56 -0300

    feat route and load database in config.go

--- configs/config.go
+++ configs/config.go
@@ -1,8 +1,15 @@
 package configs
 
 import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/jwtauth"
 	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"ms-control-point/internal/entity"
+	"ms-control-point/internal/handlers"
 )
 
 type conf struct {
@@ -37,4 +44,34 @@ func LoadConfig(path string) (*conf, error) {
 
 	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
 	return cfg, nil
+}
+
+func (c *conf) LoadDatabase() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&entity.User{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (c *conf) LoadRoutes(handler *handlers.UserHandler) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", c.TokenAuth))
+	r.Use(middleware.WithValue("expiresIn", c.JWTExpiresIn))
+
+	// User
+	r.Post("/user", handler.CreateUser)
+	r.Post("/user/generate_token", handler.GetJWT)
+
+	return r
 }
